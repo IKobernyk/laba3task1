7fed72da19b51b4844c8ed08d745f485ba13ed6d
Ірина <@DESKTOP-52NAL9S>
2024-10-15 16:38:54 +0300

    Add project files.

---
+++ lab3task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>  
+
+int main() {
+    
+    const double a = 4;
+    const double b = 3.2;
+    const double c = 0.1;
+
+    
+                   
+    
+
+    double f = 1/tan(c) + sqrt((c / b) + a);
+
+    
+    std::cout << "Function 'F' = " << f << std::endl;
+
+    return 0;
+}
